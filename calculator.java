564db82df0151031f16c6b6ecc6332624b6c7763
neerajnp047 <neerajnp047@gmail.com>
2025-01-07 16:30:20 +0530

    Division

--- calculator.java
+++ calculator.java
@@ -22,6 +22,9 @@ public class calculator {
 	  case '*':
       		System.out.println(num1 * num2);
 		break;
+          case '/':
+                System.out.println(num1 / num2);
+                break;
                
           default:
                break;
